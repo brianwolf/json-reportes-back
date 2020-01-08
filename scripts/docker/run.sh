8a7b6e8ea826884b25bb975b046feebf6e5c2a65
Brian Lobo <brian.d.lobo@gmail.com>
2020-01-07 22:26:16 -0300

    actualizo scripts de circleci

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -7,6 +7,4 @@ docker run -d --rm \
 --env-file $DOCKER_ARCHIVO_AMBIENTE \
 --name $DOCKER_NOMBRE_IMAGEN \
 -p $DOCKER_PUERTO_EXTERNO:$DOCKER_PUERTO_INTERNO \
--v $PWD/src/archivos:/usr/src/archivos \
--v $PWD/src/logs:/usr/src/logs \
 $DOCKER_USER/$DOCKER_NOMBRE_IMAGEN:$DOCKER_TAG
